a1547ccabc03a85cef8249e3855a5bcbe1b7fdaf
Javier Miranda <jmiranda@gloin.es>
2025-07-03 21:04:56 +0200

    Fix role creation trigger

--- supabase/migrations/20250703181500_user_roles.sql
+++ supabase/migrations/20250703181500_user_roles.sql
@@ -6,14 +6,28 @@ create table "public"."user_roles" (
 
 alter table "public"."user_roles" enable row level security;
 
+grant delete on table "public"."user_roles" to "service_role";
+grant insert on table "public"."user_roles" to "service_role";
+grant references on table "public"."user_roles" to "service_role";
+grant select on table "public"."user_roles" to "service_role";
+grant trigger on table "public"."user_roles" to "service_role";
+grant truncate on table "public"."user_roles" to "service_role";
+grant update on table "public"."user_roles" to "service_role";
+
 create policy "Users can view own role" on "public"."user_roles"
     for select using (auth.uid() = id);
 
 create policy "Users can insert own role" on "public"."user_roles"
     for insert with check (auth.uid() = id);
 
+create policy "Service can insert role" on "public"."user_roles"
+    for insert to service_role with check (true);
+
 create function public.handle_new_user()
-returns trigger language plpgsql as $$
+returns trigger
+language plpgsql
+security definer
+as $$
 begin
   insert into public.user_roles(id) values(new.id);
   return new;
